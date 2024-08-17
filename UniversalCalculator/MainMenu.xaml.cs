a6e032cd71643d85009d60b348a86fcde08b3850
Caroline Chiefari <caroline.chiefari@student.tafesa.edu.au>
2024-08-17 13:39:49 +0930

    Added MainMenu.xaml, MainMenu.xaml.cs and abacus.PNG

---
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class MainMenu : Page
+	{
+		public MainMenu()
+		{
+			this.InitializeComponent();
+		}
+
+		private void mathsButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void mortgageButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void currencyButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void tripButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void exitButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
